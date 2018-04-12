fe62ed518af34a23d18248484827599f8f7a3ba9
Plaga-Devs <ContactPlaga2018@gmail.com>
2018-04-12 10:42:57 -0500

    Part 4 complete
    AttributeError at /
    type object 'Post' has no attribute 'object'
    Request Method:	GET
    Request URL:	http://127.0.0.1:8000/
    Django Version:	1.10
    Exception Type:	AttributeError
    Exception Value:
    type object 'Post' has no attribute 'object'
    Exception Location:	/Users/rl012005/Desktop/PlagaProject/Plaga-Blog/plaga_site/blog/views.py in get_queryset, line 28
    Python Executable:	/Users/rl012005/anaconda3/bin/python
    Python Version:	3.6.2
    Python Path:
    ['/Users/rl012005/Desktop/PlagaProject/Plaga-Blog/plaga_site',
     '/Users/rl012005/anaconda3/lib/python36.zip',
     '/Users/rl012005/anaconda3/lib/python3.6',
     '/Users/rl012005/anaconda3/lib/python3.6/lib-dynload',
     '/Users/rl012005/anaconda3/lib/python3.6/site-packages',
     '/Users/rl012005/anaconda3/lib/python3.6/site-packages/aeosa']
    Server time:	Thu, 12 Apr 2018 15:39:05 +0000
    
    Still getting this error locally

--- plaga_site/blog/views.py
+++ plaga_site/blog/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
-
+from blog.models import Post,Comment
+from django.urls import reverse_lazy
+from blog.forms import PostForm, CommentForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView)
 # Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    '''
+    a query on my model
+    grab a post model and objects there and filter based on publisheddate__lte("less than or equal too")
+    the current time. then order by published_date.  -published (descending because of the "-")
+
+    This is called a field lookup in Django documentation.
+    https://docs.djangoproject.com/en/1.10/topics/db/queries
+
+    SQL equevalent
+    # SELECT * FROM blog_entry WHERE pub_date <= '2006-01-01';
+
+    '''
+    def get_queryset(self):
+        return Post.object.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
+
+# creates a new post with login required
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+# updates your post with login required
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+# This is the draft that is stored before they are submitted
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+
+    redirect_field_name = 'blog/post_list'
+    model = Post
+
+# Checking for no publish_date
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
