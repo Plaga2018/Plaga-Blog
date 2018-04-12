95f488c067b3549d102bdf592f021655ea02f7ce
Plaga-Devs <ContactPlaga2018@gmail.com>
2018-04-12 11:14:02 -0500

    Part Five Complete
    Server is running locally
    
    Still receiving error in browser hoping to resolve this in the next few parts.
    AttributeError at /
    type object 'Post' has no attribute 'object'
    Request Method:	GET
    Request URL:	http://127.0.0.1:8000/
    Django Version:	1.10
    Exception Type:	AttributeError
    Exception Value:
    type object 'Post' has no attribute 'object'
    Exception Location:	/Users/rl012005/Desktop/PlagaProject/Plaga-Blog/plaga_site/blog/views.py in get_queryset, line 30
    Python Executable:	/Users/rl012005/anaconda3/bin/python
    Python Version:	3.6.2
    Python Path:
    ['/Users/rl012005/Desktop/PlagaProject/Plaga-Blog/plaga_site',
     '/Users/rl012005/anaconda3/lib/python36.zip',
     '/Users/rl012005/anaconda3/lib/python3.6',
     '/Users/rl012005/anaconda3/lib/python3.6/lib-dynload',
     '/Users/rl012005/anaconda3/lib/python3.6/site-packages',
     '/Users/rl012005/anaconda3/lib/python3.6/site-packages/aeosa']
    Server time:	Thu, 12 Apr 2018 16:13:10 +0000

--- plaga_site/blog/views.py
+++ plaga_site/blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.utils import timezone
 from blog.models import Post,Comment
 from django.urls import reverse_lazy
 from blog.forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView)
 # Create your views here.
@@ -59,3 +61,46 @@ class DraftListView(LoginRequiredMixin,ListView):
 # Checking for no publish_date
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+
+
+##################################################################
+##################################################################
+#Publish View
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish
+    return redirect('post_detail')
+
+#Comment views
+
+@login_required
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.Post)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request,'blog/comment_form.html',{'form':form})
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    # pulled from the class Comment.approve() function
+    comment.approve()
+    #this comes from the Comment then to the Post
+    return redirect('post_detail',pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    #extra variable so we dont delete before removing
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk)
